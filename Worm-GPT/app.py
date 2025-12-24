d9100b9915d5d661c23e8d29c41e0e2646a4619f
ahmednasr21st <hoga74161@gmail.com>
2025-12-24 13:24:59 +0200

    Update app.py

--- Worm-GPT/app.py
+++ Worm-GPT/app.py
@@ -86,7 +86,7 @@ if not st.session_state.authenticated:
 
 # --- 4. محرك الرد غير المقيد (Unfiltered Cyber Core) ---
 # ضع مفاتيحك هنا لتجنب الـ Limit
-MY_APIS = ["AIzaSyAwdFvsvb24X2b2yxNm3D5cIqDAQzyfxSo"]
+
 
 def cyber_engine(prompt):
     # استخدام الموديلات القوية التي تظهر في حسابك
